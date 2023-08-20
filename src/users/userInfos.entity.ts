40cd2492ed5274f016a38729a4a373b6e83add80
MOZZ\admin <yeong0319@naver.com>
2023-08-20 23:19:26 +0900

    init : user, userInfo엔티티 추가

---
+++ src/users/userInfos.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Users } from './users.entity';
+
+@Entity()
+export class UserInfos {
+  @PrimaryGeneratedColumn()
+  userInfoId: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  nickname: string;
+
+  @Column()
+  profileUrl: string;
+
+  @Column()
+  myStatus: string;
+
+  @OneToOne(() => Users, user => user.userInfo)
+  user: Users;
+}
