4e85de6f870518cb890ad7a4997cf062eff0f3c3
MOZZ\admin <yeong0319@naver.com>
2023-08-22 19:22:48 +0900

    feat : jwt인증 미들웨어 추가

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from 'src/users/users.entity';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Users]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWTKEY,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [JwtStrategy],
+  exports: [JwtStrategy, PassportModule, JwtModule],
+})
+export class AuthModule {}
---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,31 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Repository } from 'typeorm';
+import { Users } from 'src/users/users.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(Users)
+    private userRepository: Repository<Users>,
+  ) {
+    super({
+      secretOrKey: process.env.JWTKEY,
+      jwtFormRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+  async validate(payload) {
+    const { userId } = payload;
+    const user: Users = await this.userRepository.findOne({
+      where: { userId },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('토큰 인증 문제');
+    }
+
+    return user;
+  }
+}
