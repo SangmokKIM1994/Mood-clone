e879511328d5957ef723443354bc7cadc71830eb
MOZZ\admin <yeong0319@naver.com>
2023-08-25 20:01:06 +0900

    fix : id와 nickname에 unique설정

--- src/users/userInfos.entity.ts
+++ src/users/userInfos.entity.ts
@@ -1,4 +1,10 @@
-import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
 import { Users } from "./users.entity";
 
 @Entity()
@@ -9,7 +15,7 @@ export class UserInfos {
   @Column()
   email: string;
 
-  @Column()
+  @Column({ unique: true })
   nickname: string;
 
   @Column()
