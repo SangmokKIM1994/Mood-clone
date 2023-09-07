160752dd105e77fca207278ed52d036e2b168b91
MOZZ\admin <yeong0319@naver.com>
2023-09-07 20:35:25 +0900

    feat : 음악 추천을 위한 openai api추가

---
+++ src/openai/openai.client.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import OpenAI from "openai";
+
+@Injectable()
+export class OpenAiService {
+  private readonly openai;
+  constructor(private configService: ConfigService) {
+    this.openai = new OpenAI({
+      apiKey: this.configService.get<string>("OPENAI_SECRET_KEY"),
+    });
+  }
+  async generateText(prompt: string): Promise<string> {
+    try {
+      const response = await this.openai.createCompletion({
+        engine: "gpt-3.5-turbo",
+        prompt: prompt,
+        max_tokens: 50,
+      });
+      console.log(response);
+      return response.choices[0].text;
+    } catch (error) {
+      throw new Error(`Failed to generate text: ${error.message}`);
+    }
+  }
+}
