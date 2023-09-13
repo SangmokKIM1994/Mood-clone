93f9e95d23d5bc94703456a5af33c57fef2b5ebe
MOZZ\admin <yeong0319@naver.com>
2023-09-13 18:36:05 +0900

    fix : music.module에 의존성 주입 추가

--- src/music/music.module.ts
+++ src/music/music.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MusicController } from './music.controller';
 import { MusicService } from './music.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserInfos } from 'src/users/userInfos.entity';
+import { Users } from 'src/users/users.entity';
+import { Musics } from './music.entity';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Users,UserInfos,Musics])],
   controllers: [MusicController],
-  providers: [MusicService]
+  providers: [MusicService, UsersService]
 })
 export class MusicModule {}
