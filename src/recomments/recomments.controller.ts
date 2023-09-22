a19b82d3b83332d8d3bdb587007fce43a8c0905d
MOZZ\admin <yeong0319@naver.com>
2023-09-22 20:27:57 +0900

    feat : 대댓글 생성 기능 추가

--- src/recomments/recomments.controller.ts
+++ src/recomments/recomments.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  UseGuards,
+  Post,
+  Param,
+  Req,
+  Body,
+  ValidationPipe,
+} from "@nestjs/common";
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { RecommentsService } from "./recomments.service";
+import { CreateRecommentDto } from "./dto/create.recomment.dto";
+import { AuthGuard } from "@nestjs/passport";
+import { Users } from "src/users/users.entity";
+import { Request as ExpressRequest } from "express";
 
-@Controller('recomments')
-export class RecommentsController {}
+@ApiTags("recomments")
+@Controller("recomments")
+export class RecommentsController {
+  constructor(private readonly recommentService: RecommentsService) {}
+
+  @ApiOperation({ summary: "대댓글 생성" })
+  @ApiBody({ type: CreateRecommentDto })
+  @ApiResponse({ status: 201, description: "댓글 생성 완료" })
+  @UseGuards(AuthGuard("jwt"))
+  @Post("/")
+  async createCommnet(
+    @Param() commentId: number,
+    @Req() req: ExpressRequest & { user: Users },
+    @Body(ValidationPipe) createReommentDto: CreateRecommentDto
+  ) {
+    const { recomment } = createReommentDto;
+    const user = req.user;
+    await this.recommentService.createRecomment({ user, commentId, recomment });
+  }
+}
