24254e838fefb2a505cc8375ec8b7d85698333c4
MOZZ\admin <yeong0319@naver.com>
2023-09-23 19:21:56 +0900

    feat : 대댓글 수정과 삭제 기능 추가, 수정을 위한 dto생성

--- src/recomments/recomments.controller.ts
+++ src/recomments/recomments.controller.ts
@@ -6,6 +6,8 @@ import {
   Req,
   Body,
   ValidationPipe,
+  Patch,
+  Delete,
 } from "@nestjs/common";
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { RecommentsService } from "./recomments.service";
@@ -32,5 +34,37 @@ export class RecommentsController {
     const { recomment } = createReommentDto;
     const user = req.user;
     await this.recommentService.createRecomment({ user, commentId, recomment });
+  }
+
+  @ApiOperation({ summary: "대댓글 수정" })
+  // @ApiBody({ type: CreateCommentDto })
+  @ApiResponse({ status: 200, description: "대댓글 수정 완료" })
+  @UseGuards(AuthGuard("jwt"))
+  @Patch(":recommentId")
+  async updateComment(
+    @Param() recommentId: number,
+    @Req() req: ExpressRequest & { user: Users },
+    @Body(ValidationPipe) updateRecomment: string
+  ) {
+    const user = req.user;
+    await this.recommentService.updateRecomment({
+      recommentId,
+      updateRecomment,
+      user,
+    });
+    return;
+  }
+
+  @ApiOperation({ summary: "대댓글 삭제" })
+  // @ApiBody({ type: CreateCommentDto })
+  @ApiResponse({ status: 200, description: "대댓글 삭제 완료" })
+  @UseGuards(AuthGuard("jwt"))
+  @Delete(":recommentId")
+  async deleteComment(
+    @Param() recommentId: number,
+    @Req() req: ExpressRequest & { user: Users }
+  ) {
+    await this.recommentService.deleteRecomment(req.user, recommentId);
+    return;
   }
 }
