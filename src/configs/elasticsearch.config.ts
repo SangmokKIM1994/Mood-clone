9d8a386fae4ff95515a44ab26c947be002a6240b
MOZZ\admin <yeong0319@naver.com>
2023-10-08 18:15:03 +0900

    init : 음악 검색을 위한 elasticsearch기본 설정 추가, 아직 aws에서 생성 전

---
+++ src/configs/elasticsearch.config.ts
@@ -0,0 +1,8 @@
+export const elasticsearchConfig = {
+    node: 'https://your-aws-elasticsearch-endpoint',
+    awsConfig: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      region: process.env.AWS_REGION,
+    },
+  };
