51a7878826369ffea5fce01ba256fd67543a6351
unknown <yeong0319@naver.com>
2023-10-14 13:50:05 +0900

    fix : status와 스크랩읠 관계를 형성해서 A기분일떄 이 스크랩이 생성 이런 자료를 남기고 싶었음

--- src/music/status.entity.ts
+++ src/music/status.entity.ts
@@ -1,5 +1,6 @@
 import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Musics } from "./music.entity";
+import { Scraps } from "src/scraps/scraps.entity";
 
 @Entity()
 export class Status {
@@ -11,4 +12,7 @@ export class Status {
 
   @ManyToMany(() => Musics, (music) => music.status)
   music: Musics[];
+
+  @ManyToMany(() => Scraps, (scrap) => scrap.status)
+  scrap:Scraps[]
 }
