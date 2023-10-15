f9f61b4b99bd64b2258cd61dcc462c021d94692d
MOZZ\admin <yeong0319@naver.com>
2023-10-15 19:38:18 +0900

    fix : 어떤기분에서 좋아요를 했는지 알수있게 like와 status의 관계설정

--- src/music/status.entity.ts
+++ src/music/status.entity.ts
@@ -1,6 +1,7 @@
 import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Musics } from "./music.entity";
 import { Scraps } from "src/scraps/scraps.entity";
+import { Likes } from "src/likes/likes.entity";
 
 @Entity()
 export class Status {
@@ -14,5 +15,8 @@ export class Status {
   music: Musics[];
 
   @ManyToMany(() => Scraps, (scrap) => scrap.status)
-  scrap:Scraps[]
+  scrap: Scraps[];
+
+  @ManyToMany(() => Likes, (like) => like.status)
+  like: Likes[];
 }
