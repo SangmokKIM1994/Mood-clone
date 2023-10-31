ee856a4a99b8e86220866c1e97652422b863360e
MOZZ\admin <yeong0319@naver.com>
2023-10-31 19:45:55 +0900

    fix : 음악 맞춤 추천 controller랑 service 연결

--- src/music/music.controller.ts
+++ src/music/music.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import { Controller, Get, Param, Req, UseGuards } from "@nestjs/common";
 import { ApiOperation, ApiResponse } from "@nestjs/swagger";
 import { MusicService } from "./music.service";
 import { Users } from "src/users/users.entity";
+import { Request as ExpressRequest } from "express";
+import { AuthGuard } from "@nestjs/passport";
 
 @Controller("music")
 export class MusicController {
@@ -9,10 +11,14 @@ export class MusicController {
 
   @ApiOperation({ summary: "음악 조회" })
   @ApiResponse({ status: 200, description: "음악 조회 완료" })
+  @UseGuards(AuthGuard("jwt"))
   @Get("/:musicId")
-  async findMusicByMusicId(@Param() musicId: number) {
-    // const user = Users;
-    // const music = await this.musicService.findMusicByMusicId(musicId, user);
-    // return music;
+  async findMusicByMusicId(
+    @Param() musicId: number,
+    @Req() req: ExpressRequest & { user: Users }
+  ) {
+    const user = req.user;
+    const music = await this.musicService.findMusicByMusicId(musicId, user);
+    return music;
   }
 }
